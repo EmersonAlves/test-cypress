d1121043cb5a3e2f6ac94bcf877b1ebb84f1bd4b
Emerson Alves <emerson.jx13@gmail.com>
2021-01-28 21:39:31 -0300

    readjustment in the test category

--- cypress/integration/examples/form.spec.js
+++ cypress/integration/examples/form.spec.js
@@ -1,27 +1,34 @@
 describe("Validar formulario", () => {
   it("Visitar site", () => {
     cy.visit("https://fabrica704.fabrica704.com.br/cliente/preregister");
+  });
 
+  it("Validar input de nome", () => {
     cy.get("#name")
       .type("Maria Aparecida")
       .should("have.value", "Maria Aparecida");
+  });
 
+  it("Validar input de email", () => {
     cy.get("#email")
       .type("mariaap@test.com")
       .should("have.value", "mariaap@test.com");
+  });
 
+  it("Validar input de telefone", () => {
     cy.get("#telphone")
       .type("85988725804")
       .should("have.value", "(85) 98872-5804");
+  });
 
+  it("Validar select de genero", () => {
     cy.get("input.select-dropdown").click();
     cy.get("ul.select-dropdown > li:nth-child(3)").click();
 
-    cy.get('select').should('have.value', 'feminino');
-
-    
-    cy.get("#password")
-      .type("123456")
-      .should("have.value", "123456");
+    cy.get("select").should("have.value", "feminino");
+  });
+  
+  it("Validar input de senha", () => {
+    cy.get("#password").type("123456").should("have.value", "123456");
   });
 });
